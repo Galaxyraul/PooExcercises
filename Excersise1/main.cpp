79f58a6527dad9d2fa8ac1a8c277dc2679c7f4cc
Galaxyraul <cocolete03@gmail.com>
2022-02-26 18:31:32 +0100

    FLowerPot

--- Excersise1/main.cpp
+++ Excersise1/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "Pencil.h"
 #include "Book.h"
+#include "FlowerPot.h"
 
 int main() {
     Pencil p1;
@@ -9,7 +10,7 @@ int main() {
     p1.setColour("Black");
     p1.setHardness("HB4");
     Pencil p3(p2);
-    std::cout << p3.getSize() << std::endl;
+    std::cout << "The size of the pencil is:" << p3.getSize() << " mm" << std::endl;
     
     Book b1("Cixin Liu", "The three-body problem", "Sci-Fi", 2006, 20);
     Book b2;
@@ -19,5 +20,11 @@ int main() {
     b2.setType("Fantasy");
     b2.setPublicationYear(1998);
     Book b3(b2);
-    std::cout<< "The author of " + b3.getName() + " is " + b3.getAuthor();
+    std::cout<< "The author of " + b3.getName() + " is " + b3.getAuthor() << std::endl;
+
+    FlowerPot f1;
+    FlowerPot f2("clay","Red",3);
+    FlowerPot f3(f2);
+    std::cout<< "The pot has a capacity of:" << f3.getCapacity() << " L" << std::endl;
+
 }
