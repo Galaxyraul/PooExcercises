3e027f0329fa0981742b31a90831168b4c98c578
Galaxyraul <cocolete03@gmail.com>
2022-02-26 12:29:03 +0100

    Pencil

--- Excersise1/Pencil.cpp
+++ Excersise1/Pencil.cpp
@@ -6,9 +6,10 @@
 
 Pencil::Pencil() {}
 
-Pencil::Pencil(const std::string &hardness, const std::string &colour, const int size):_hardness(hardness),_colour(colour),_size(size) {}
+Pencil::Pencil(const std::string &hardness, const std::string &colour, const int size)
+                :_hardness(hardness),_colour(colour),_size(size) {}
 
-Pencil::Pencil(const Pencil &orig) {}
+Pencil::Pencil(const Pencil &orig):_hardness(orig._hardness),_colour(orig._colour),_size(orig._size) {}
 
 std::string Pencil::getHardness(){
     return _hardness;
